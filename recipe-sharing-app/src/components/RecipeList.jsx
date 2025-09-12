00f8079cdd6bdfb10f0885ea32f2e402d97c59ec
kashikoi2 <hakim.belman@gmail.com>
2025-09-12 15:30:40 +0100

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
-import { useRecipeStore } from '../store/recipeStore';
+import { Link } from "react-router-dom";
+import { useRecipeStore } from "../store/recipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -9,7 +10,9 @@ const RecipeList = () => {
       {recipes.length === 0 && <p>No recipes yet. Add one!</p>}
       {recipes.map((recipe) => (
         <div key={recipe.id} style={{ border: "1px solid #ccc", padding: "10px", marginBottom: "10px" }}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
@@ -18,3 +21,4 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
+
