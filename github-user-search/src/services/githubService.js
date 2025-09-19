cc7d48e450970014dca4aecc2e81b50e860dad1c
kashikoi2 <hakim.belman@gmail.com>
2025-09-19 22:30:32 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,3 +10,21 @@ export const fetchUserData = async (username) => {
     throw new Error("User not found");
   }
 };
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/search/users";
+
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos} `;
+
+  try {
+    const response = await axios.get(`${BASE_URL}?q=${encodeURIComponent(query)}&per_page=12`);
+    return response.data.items;
+  } catch (error) {
+    throw new Error("No users found");
+  }
+};
