ac4b5d9e89974e6624b54af54fc23a101bdabe27
kashikoi2 <hakim.belman@gmail.com>
2025-10-12 23:20:05 +0100

    Create ProfileDetails.jsx

---
+++ react-router-advanced/src/pages/ProfileDetails.jsx
@@ -0,0 +1,9 @@
+export default function ProfileDetails() {
+  return (
+    <div>
+      <h2 className="text-lg font-semibold">Profile Details</h2>
+      <p>Username: Momo</p>
+      <p>Email: momo@example.com</p>
+    </div>
+  );
+}
