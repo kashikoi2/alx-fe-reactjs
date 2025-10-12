63c61db8f930fb207b674ddf673a93fe7e9385b3
kashikoi2 <hakim.belman@gmail.com>
2025-10-12 23:19:18 +0100

    Create About.jsx

---
+++ react-router-advanced/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <p>This app demonstrates nested, dynamic, and protected routes in React.</p>
+    </div>
+  );
+}
