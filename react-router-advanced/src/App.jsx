4d1dbd72c51cb6afc672707a9323afa786aa5d63
kashikoi2 <hakim.belman@gmail.com>
2025-10-12 23:18:09 +0100

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,40 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
+  return (
+    <div className="p-6">
+      <nav className="flex gap-4 mb-6 border-b pb-3">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/posts/1">Blog Post (Dynamic)</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Protected Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic Route */}
+        <Route path="/posts/:id" element={<BlogPost />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
