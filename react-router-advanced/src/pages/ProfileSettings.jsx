78d64b75e5a7a2de4c10b776c239c6a7dd632832
kashikoi2 <hakim.belman@gmail.com>
2025-10-12 23:20:24 +0100

    Create ProfileSettings.jsx

---
+++ react-router-advanced/src/pages/ProfileSettings.jsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings() {
+  return (
+    <div>
+      <h2 className="text-lg font-semibold">Profile Settings</h2>
+      <p>Change your password, update profile info, and more.</p>
+    </div>
+  );
+}
